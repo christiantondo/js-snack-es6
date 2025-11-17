36de94761d815f3c3013adbd6715d8ea02101a76
christiantondo <christiantondo1@gmail.com>
2025-11-17 19:42:53 +0100

    comments and cycle

--- snack2/main.js
+++ snack2/main.js
@@ -2,21 +2,32 @@ console.log("JS ok - Snack 2")
 
 const clubs = [
 
+    // Club 1 - Inter
     {
         name: "Inter",
         points: "0",
         penalties: "0",
     },
 
+    // Club 2 - Napoli
     {
         name: "Napoli",
         points: "0",
         penalties: "0",
     },
-    
+
+    // Club 3 - Roma
     {
         name: "Roma",
         points: "0",
         penalties: "0",
     }
-]
+];
+
+const clubNames = []
+
+for (let i = 0; i < clubs.length; i++) {
+    const club = clubs[i];
+    
+}
+    
