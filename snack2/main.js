dfacab7ae6cc3383e14525b3aaac44847b5b971f
christiantondo <christiantondo1@gmail.com>
2025-11-17 19:34:10 +0100

    array of object created

--- snack2/main.js
+++ snack2/main.js
@@ -1,1 +1,22 @@
 console.log("JS ok - Snack 2")
+
+const clubs = [
+
+    {
+        name: "Inter",
+        points: "0",
+        penalties: "0",
+    },
+
+    {
+        name: "Napoli",
+        points: "0",
+        penalties: "0",
+    },
+    
+    {
+        name: "Roma",
+        points: "0",
+        penalties: "0",
+    }
+]
