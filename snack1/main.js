f6c5c57845991fed570dd30d66afabea4ad0d50e
christiantondo <christiantondo1@gmail.com>
2025-11-17 17:25:33 +0100

    array of objects created

--- snack1/main.js
+++ snack1/main.js
@@ -1,1 +1,26 @@
 console.log("JS ok - Snack 1")
+
+const racingBicycles = [
+
+    {
+        name: "bike1",
+        brand: "Bianchi",
+        weight: 6.85,
+        color: "white",
+        rider: "Micheal",
+    },
+    {
+        name: "bike2",
+        brand: "Pinarello",
+        weight: 7.60,
+        color: "black",
+        rider: "Dwight",
+    },
+    {
+        name: "bike3",
+        brand: "Trek",
+        weight: 7.52,
+        color: "red",
+        rider: "Jim",
+    }
+]
